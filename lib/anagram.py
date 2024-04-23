9c8bb4d3eb1d022c0c652dd408c16193e091005b
Lyle Simmons <lylesimmons125@gmail.com>
2024-04-23 16:10:08 -0400

    lab complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        matches = []
+        sorted_word = sorted(self.word.lower())
+        for word in word_list:
+            if sorted(word.lower()) == sorted_word and word.lower() != self.word.lower():
+                matches.append(word)
+        return matches
